56a67a68b20db43e2e5c8e0b98f1d8355c5a6e88
mcdermatt <mcdermatt@gmail.com>
2020-02-07 12:34:41 -0800

    1d plot test

--- HW1/1Dplot.py
+++ HW1/1Dplot.py
@@ -1,9 +1,27 @@
 from matplotlib import pyplot as plt
 import numpy as np
-
 plt.figure()
-a = [1,2,5,6,9,11,15,17,18]
-plt.hlines(1,1,20)  # Draw a horizontal line
-plt.eventplot(a, orientation='horizontal', colors='b')
-plt.axis('off')
-plt.show()
+
+x = 0
+dx = 0
+ddx = 0
+X = [x,dx,ddx]
+t = 0
+runlen = 100
+
+plt.hlines(0,-20,20)  # Draw a horizontal line
+plt.yticks([])
+plt.draw()
+
+while t < runlen:
+	#random change in acceleration fom -1 to 1
+	ddx = np.random.randn(1)
+	dx = dx + ddx
+	x = x + dx + 0.5*ddx
+	print(x,dx,ddx)
+	boatpos, = plt.plot(x,0,'ro')
+	#plt.eventplot(a, orientation='horizontal', colors='b') # plots vertical lines
+	plt.pause(0.05)
+	boatpos.remove()
+	plt.draw()
+	t += 1
