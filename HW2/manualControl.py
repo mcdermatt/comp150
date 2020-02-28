20df3c1b8ce8a0ba823d6e3ff9f7c9db6e935623
mcdermatt <mcdermatt@gmail.com>
2020-02-28 15:07:31 -0800

    particle filter now working across mutiple timesteps, still need to work on drift

--- HW2/manualControl.py
+++ HW2/manualControl.py
@@ -3,9 +3,12 @@ import cv2
 from matplotlib import pyplot as plt
 
 #number of initial particles in each direction
-fidelity = 25
+fidelity = 20
 droneFOV = 70 #length of sides of square image taken by drone camera
 particleNoise = 5
+movementNoise = 15
+speed = 15
+runtime = 30
 
 #Main Map
 plt.figure(0)
@@ -38,105 +41,124 @@ for i in x:
 dronex = int(np.floor(np.random.rand()*(img.shape[0] - droneFOV)))  
 droney = int(np.floor(np.random.rand()*(img.shape[1] - droneFOV)))
 print("Actual Coords of Drone: ",dronex, " ", droney)
-#plot image taken by drone camera
-plt.figure(1)
-dronePic = img[dronex:dronex+droneFOV,droney:droney+droneFOV]
-#get negative of drone camera image
-#dronePic = cv2.bitwise_not(dronePic)
+
+runNum = 1
+while runNum <= runtime:
+	#plot image taken by drone camera
+	plt.figure(1)
+	plt.clf()
+	dronePic = img[int(dronex):int(dronex+droneFOV),int(droney):int(droney+droneFOV)]
+	#get negative of drone camera image
+	#dronePic = cv2.bitwise_not(dronePic)
+	plt.imshow(dronePic,cmap = 'gray', interpolation = 'bicubic')
+	plt.pause(0.05)
+	plt.draw()
+
+	#loop through particle positions drawing cropped map in figure 2
+	worstFit = 255*(fidelity**2)
+	fitness = np.zeros([fidelity**2])
+	cumFitness = np.zeros([fidelity**2])
+	bestFit = 0
+	bestPt = 0
+
+	for k in particlePosArr[0]:
+		#Cropped Map
+		# plt.figure(2)
+		imgCropped = img[int(particlePosArr[1,int(k)-1]):int(particlePosArr[1,int(k)-1]+droneFOV),int(particlePosArr[2,int(k)-1]):int(particlePosArr[2,int(k)-1]+droneFOV)]
+		# plt.imshow(imgCropped, cmap = 'gray', interpolation = 'bicubic')
+
+		#record closest matching particle numbers
+		# plt.draw()
+		# plt.pause(0.05)
+		# plt.clf()
+		if (imgCropped.shape == dronePic.shape):
+			#compare cropped image to inverse drone image (find difference)
+			overlap = np.sum(np.subtract(imgCropped, dronePic))
+			fitness[int(k)-1] = worstFit - overlap
+		else:
+			#set arbitrarily low value - if you set to zero point will never be looked at again
+			fitness[int(k)-1] = 0
+
+		if fitness[int(k)-1] > bestFit:
+			bestFit = fitness[int(k)-1]
+			bestPt = int(k)
+			print("Best Match is ", int(k))
+
+		#main map
+		#plt.figure(0)
+		#display where current search point is
+
+#	print(fitness)
+	#weigh fitness of points for resampling
+	sumOfFitness = np.sum(fitness)
+	#print("sum of fitness: ", sumOfFitness)
+	cumFitness[0] = fitness[0]
+	m = 0
+	while m < fitness.shape[0]:
+		fitness[m] = fitness[m]/sumOfFitness
+		cumFitness[m] = fitness[m] + cumFitness[m-1]
+		m += 1
+
+	#pick new points to resample based on cumFitness
+	n = 0
+	while n < particlePosArr.shape[1]:
+		r = np.random.rand() #random variable
+		#check to see which value in cumFitness got picked
+		p = 0
+		while p < fitness.shape[0]:
+			if cumFitness[p] > r:
+				#update particlePosArray with value closes to randomly chosen point + noise
+				particlePosArr[1,n] = particlePosArr[1,p] + np.floor(particleNoise*np.random.randn())
+				particlePosArr[2,n] = particlePosArr[2,p] + np.floor(particleNoise*np.random.randn())
+				break
+			p += 1
+		n += 1
 
 
-plt.imshow(dronePic,cmap = 'gray', interpolation = 'bicubic')
+	#print(particlePosArr)
 
 
+	#update map with particles
+	plt.figure(0)
+	plt.clf()
+	plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
+	plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+	# plt.xlim((img.shape[1],0))
+	# plt.ylim((img.shape[0],0))
+	# u = 0
+	# while u < particlePosArr.shape[1]:
+	# 	plt.plot(particlePosArr[2,u],particlePosArr[1,u],'b.')
+	# 	u+=1
+	#plt.plot(particlePosArr[1,:],particlePosArr[2,:],'b.')
+	#plt.pause(0.05)
+	#plt.draw()
 
-#loop through particle positions drawing cropped map in figure 2
-worstFit = 255*(fidelity**2)
-fitness = np.zeros([fidelity**2])
-cumFitness = np.zeros([fidelity**2])
-bestFit = 0
-bestPt = 0
+	print("most likely coords of drone: ", particlePosArr[:,bestPt])
+	#draw most likely estimated location on main map
+	#plt.plot(particlePosArr[2,bestPt],particlePosArr[1,bestPt],'go')
+	#plt.pause(0.5)
+	#plt.draw()
 
-for k in particlePosArr[0]:
-	#Cropped Map
-	# plt.figure(2)
-	imgCropped = img[int(particlePosArr[1,int(k)-1]):int(particlePosArr[1,int(k)-1]+droneFOV),int(particlePosArr[2,int(k)-1]):int(particlePosArr[2,int(k)-1]+droneFOV)]
-	# plt.imshow(imgCropped, cmap = 'gray', interpolation = 'bicubic')
-
-	#record closest matching particle numbers
-	# plt.draw()
-	# plt.pause(0.05)
-	# plt.clf()
-	if (imgCropped.shape == dronePic.shape):
-		#compare cropped image to inverse drone image (find difference)
-		overlap = np.sum(np.subtract(imgCropped, dronePic))
-		fitness[int(k)-1] = worstFit - overlap
-	else:
-		#set arbitrarily low value for now
-		fitness[int(k)-1] = 0
-
-	if fitness[int(k)-1] > bestFit:
-		bestFit = fitness[int(k)-1]
-		bestPt = int(k)
-		print("Best Match is ", int(k))
-
-	#main map
-	#plt.figure(0)
-	#display where current search point is
-
-
-#weigh fitness of points for resampling
-sumOfFitness = np.sum(fitness)
-#print("sum of fitness: ", sumOfFitness)
-cumFitness[0] = fitness[0]
-m = 0
-while m < fitness.shape[0]:
-	fitness[m] = fitness[m]/sumOfFitness
-	cumFitness[m] = fitness[m] + cumFitness[m-1]
-	m += 1
-
-#pick new points to resample based on cumFitness
-n = 0
-while n < particlePosArr.shape[1]:
-	r = np.random.rand() #random variable
-	#check to see which value in cumFitness got picked
-	p = 0
-	while p < fitness.shape[0]:
-		if cumFitness[p] > r:
-			#update particlePosArray with value closes to randomly chosen point + noise
-			particlePosArr[1,n] = particlePosArr[1,p] + np.floor(particleNoise*np.random.randn())
-			particlePosArr[2,n] = particlePosArr[2,p] + np.floor(particleNoise*np.random.randn())
-			break
-		p += 1
-	n += 1
-
-
-#print(particlePosArr)
-
-
-#update map with particles
-plt.figure(0)
-plt.clf()
-plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
-plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
-plt.xlim((img.shape[1],0))
-plt.ylim((img.shape[0],0))
-u = 0
-while u < particlePosArr.shape[1]:
-	plt.plot(particlePosArr[2,u],particlePosArr[1,u],'r.')
-	u+=1
-plt.pause(0.05)
-plt.draw()
-
-print("most likely coords of drone: ", particlePosArr[:,bestPt])
-#draw most likely estimated location on main map
-#plt.plot(particlePosArr[2,bestPt],particlePosArr[1,bestPt],'go')
-plt.pause(0.05)
-plt.draw()
-
-#generate new particles
-
-#movement
-
+	#movement
+	theta = 2*np.pi*np.random.rand()
+	#print("theta = ", theta)
+	dx = speed*np.cos(theta)
+	dy = speed*np.sin(theta)
+	#don't go outside the map
+	if (dronex + dx > img.shape[0] - droneFOV) or (droney + dy > img.shape[0] - droneFOV):
+		dx = 0
+		dy = 0
+	#update drone position
+	dronex = dronex + dx
+	droney = droney + dy
+	#update pos array
+	particlePosArr[1,:] = particlePosArr[1,:] + dx + movementNoise
+	particlePosArr[2,:] = particlePosArr[2,:] + dy + movementNoise
+	z, = plt.plot(particlePosArr[1,:],particlePosArr[2,:],'g.')
+	plt.draw()
+	plt.pause(0.05)
+	z.remove()
+	runNum += 1
 
 plt.show()
 
