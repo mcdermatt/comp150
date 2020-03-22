77da823332781966a192f39fd552bbdefa337d37
mcdermatt <mcdermatt@gmail.com>
2020-03-22 14:40:24 -0700

    worked on lots of linalg stuff

--- HW1/kalman.py
+++ HW1/kalman.py
@@ -5,13 +5,15 @@ from matplotlib.patches import Ellipse
 
 class kalman:
 	"""creates kalman filter about specified params"""
-	A = np.array([1,1])
-	B = np.array([1,0])
+	A = np.array([[1,1],[0.5,1]])
+	B = np.array([[1],[0]])
 	C = np.array([1,0])
+	u = np.array([0,0])
+
 	startPos = 0
 	startVel = 0
 
-	def __init__(self,A = A, B = B, C=C, dim = 1, w = 0, v = 0, u=0):
+	def __init__(self,A = A, B = B, C=C, dim = 1, w = 0, v = 0, u=u):
 		self.dimension = dim #defaults to 1d case
 		self.pnoise = w #procecss noise (wind etc.)
 		self.mnoise = v #measurement noise (gps error etc)
@@ -21,7 +23,8 @@ class kalman:
 		#self.lastEst = 0
 		#self.prevVar = 10
 		#self.u = u
-		self.Q = 0
+		self.Q = np.identity(dim+1)
+		self.R = np.identity(dim+1)
 		self.I = np.identity(dim+1)
 
 		#Q = covariance matrix of noise in states
@@ -31,27 +34,37 @@ class kalman:
 		"""prediction step of kalman filter"""
 
 		#predicted position for next step
-		priori = self.A*lastEst + self.B*u
-
+		priori = np.dot(self.A,(lastEst)) + np.dot(u,self.B) #ToDo - flip B and u
+	
 		#predicted variance for next step
-		predVar	= self.A*prevVar*(self.A.transpose()) + self.Q
-
-		return(priori,predVar)
+		# predVar	= self.A*prevVar*self.A.transpose() #+ self.Q
+		predVar = self.A.dot(prevVar).dot(self.A.transpose())
+		
+		#prediction results
+		predRes = [priori,predVar]
+		return(predRes)
 
 	def update(self, priori, predVar):
 		"""update step of kalman filter"""
 		#kalman gain(?)
-		K = (predVar * self.C.transpose())/(self.C*predVar*self.C.transpose()+self.R)
+		#print(self.C.dot(predVar).dot(self.C.transpose())) #debug
+		print(predVar.dot(self.C.transpose())) #this is resulting in a constant- 
+			#do I need to transpose predVar??
+		K = np.divide((predVar.dot(self.C.transpose())),(self.C.dot(predVar).dot(self.C.transpose()) ))#+self.R))
+		print(K)
 
 		#posterior estimate
-		y = np.array([1,0])*x
+		y = np.array([1,0])*priori
 		post = priori + K*(y - self.C*priori)
 		
 		#estimated current variance
 		curVar = (self.I - K*self.C)*predVar
 
-		return(post, curVar)
+		#results of update
+		updateRes = np.array([post,curVar])
+		return(updateRes)
 
 	def display(self):
 		"""shows values on plot"""
-		print(self.A)
+		#print(self.A)
+		pass
--- HW1/main.py
+++ HW1/main.py
@@ -1,27 +1,35 @@
 from kalman import kalman
 import numpy as np
 
-A = np.array([6,9])
-B = np.array([9,6])
+A = np.array([[1,1],[0.5,1]])
+B = np.array([[1],[0]])
+C = np.array([1,0])
 
 kal = kalman(A,B,dim=1)
 
 kal.display()
 
-u = 0
-lastEst = 0
-prevVar = 10
+u = np.array([[0,0]])
+lastEst = np.array([[0],[0]]) #should be array(?)
+prevVar = np.array([10,10]) #also not sure if this should be array or scalar
 
 count = 0
-while count < 10:
+while count < 1:
 
-	priori, predVar = kal.prediction(lastEst, prevVar, u)
-	print('priori = ',priori, '  ','predVar = ', predVar)
+	#run prediction step
+	predRes = kal.prediction(lastEst, prevVar, u)
+	#print('priori = ',predRes[0], '  ','predVar = ', predRes[1])
+	priori = predRes[0]
+	predVar = predRes[1]
+	print(priori,' ',predVar)
 
+	#run update step
+	updateRes = kal.update(priori,predVar)
+	#print('post = ', updateRes[0], '   curVar = ', updateRes[1])
+	post = updateRes[0]
+	curVar = updateRes[1]
+
+	#store current values as previous step values
 	lastEst = post
 	prevVar = curVar
-
-	post, curVar = kal.update(priori,predVar)
-	print('post = ', post, '   curVar = ', curVar)
-
 	count += 1
